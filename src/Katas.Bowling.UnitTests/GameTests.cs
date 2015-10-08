f9dc78f72a16848f885171e2cf878bb27fea8c2c
Todd Meinershagen <todd@meinershagen.net>
2015-10-08 13:29:24 -0500

    Resulting code from 10/8/2015 dojo session.

--- src/Katas.Bowling.UnitTests/GameTests.cs
+++ src/Katas.Bowling.UnitTests/GameTests.cs
@@ -9,15 +9,59 @@ namespace Katas.Bowling.UnitTests
 	[TestFixture]
 	public class GameTests
 	{
+		private Game _game;
+
+		[SetUp]
+		public void Setup()
+		{
+			_game = new Game();
+		}
+
+
 		[Test]
 		public void given_a_gutter_game_when_getting_score_then_should_return_0()
 		{
-			var game = new Game();
 			
 			Enumerable.Range(1, 20).ToList()
-				.ForEach(index => game.Roll(0));
+				.ForEach(index => _game.Roll(pins: 0));
 
-			game.Score.Should().Be(0);
+			_game.Score.Should().Be(0);
+		}
+
+		[Test]
+		public void given_roll_all_ones_when_getting_score_then_should_return_20()
+		{
+
+			Enumerable.Range(1, 20).ToList()
+				.ForEach(index => _game.Roll(pins: 1));
+
+			_game.Score.Should().Be(20);
+		}
+		[Test]
+		public void One_Spare_Test()
+		{
+
+			_game.Roll(5);
+			_game.Roll(5);
+			_game.Roll(3);
+
+			Enumerable.Range(1, 17).ToList()
+			.ForEach(index => _game.Roll(pins: 0));
+			
+
+			_game.Score.Should().Be(16);
+
+			RollMany(5,5,3,0);
+		}
+
+		private void RollMany(int numberOfRolls, int pins)
+		{
+			
+		}
+
+		private void RollMany(params int[] rolls)
+		{
+			
 		}
 	}
 }
--- src/Katas.Bowling/Game.cs
+++ src/Katas.Bowling/Game.cs
@@ -1,13 +1,43 @@
-﻿namespace Katas.Bowling
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Katas.Bowling
 {
 	public class Game
 	{
+		private readonly List<int> _rolls = new List<int>();
+
 		public void Roll(int pins)
-		{ }
+		{
+			_rolls.Add(pins);
+
+		}
 
 		public int Score
 		{
-			get { return -1; }
+			get
+			{
+				var score = 0;
+
+				for (int i = 0; i < 10; i++)
+				{
+					var frameIndex = i*2;
+					var frameScore = _rolls[frameIndex] + _rolls[frameIndex + 1];
+
+					if (frameScore == 10)
+					{
+						frameScore += _rolls[frameIndex + 2];
+					}
+
+					score += frameScore;
+				}
+
+				return score;
+			}
 		}
 	}
+
+
 }
