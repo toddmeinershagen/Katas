3c2612873de701554ec863a8312d5ce3009413ab
Todd Meinershagen <todd@meinershagen.net>
2015-10-07 23:36:09 -0500

    Added bowling game kata with 1 failing test.

---
+++ src/Katas.Bowling.UnitTests/GameTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+namespace Katas.Bowling.UnitTests
+{
+	[TestFixture]
+	public class GameTests
+	{
+		[Test]
+		public void given_a_gutter_game_when_getting_score_then_should_return_0()
+		{
+			var game = new Game();
+			
+			Enumerable.Range(1, 20).ToList()
+				.ForEach(index => game.Roll(0));
+
+			game.Score.Should().Be(0);
+		}
+	}
+}
---
+++ src/Katas.Bowling/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas.Bowling
+{
+	public class Game
+	{
+		public void Roll(int pins)
+		{ }
+
+		public int Score
+		{
+			get { return -1; }
+		}
+	}
+}
